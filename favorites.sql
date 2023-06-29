b05b348ea5a3b1e91bbf17c360910518d4369afa
Semhar Haile <shaile03@terpmail.umd.edu>
2023-06-29 19:37:45 +0000

    deleting files

--- favorites.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS engine;
-
-CREATE TABLE favorites ( 
-    id INT(8) UNSIGNED NOT NULL auto_increment, 
-    title VARCHAR(255) default NULL, 
-    artist VARCHAR(255) default NULL, 
-    PRIMARY KEY (id)  
-) AUTO_INCREMENT=1;
-
